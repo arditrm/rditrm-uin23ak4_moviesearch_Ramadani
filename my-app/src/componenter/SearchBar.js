f45b75ea6f7fc92a02a5c31b607b00beb73e2f46
arditrm <ardit.ramadani904@gmail.com>
2023-03-29 22:30:48 +0200

    slutt resultat

---
+++ my-app/src/componenter/SearchBar.js
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+
+export default function SearchBar({ setMovies }) {
+  const API_KEY = "4692388b";
+  const [searchString, setSearchString] = useState("");
+
+  const searchMovies = async () => {
+    try {
+      const response = await fetch(
+        `https://www.omdbapi.com/?apikey=${API_KEY}&s=${searchString}`
+      );
+      const data = await response.json();
+      setMovies(data.Search);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleSearch = (event) => {
+    event.preventDefault();
+    searchMovies();
+  };
+
+  return (
+    <form onSubmit={handleSearch}>
+      
+     
+    </form>
+  );
+}
--- my-app/src/componenter/search.js
+++ my-app/src/componenter/search.js
@@ -1,14 +1,13 @@
 import React, { useState, useEffect } from 'react';
-import './App.css';
 
 export default function Search() {
   const API_KEY = '4692388b';
   const [searchString, setSearchString] = useState('');
   const [movies, setMovies] = useState([]);
 
-  const searchMovies = async (query) => {
+  const searchMovies = async () => {
     try {
-      const response = await fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=${query}`);
+      const response = await fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=${searchString}`);
       const data = await response.json();
       setMovies(data.Search);
     } catch (error) {
@@ -17,16 +16,12 @@ export default function Search() {
   };
 
   useEffect(() => {
-    searchMovies('james-bond'); // fetch James Bond movies when component first mounts
+    searchMovies();
   }, []);
 
   const handleSearch = (event) => {
     event.preventDefault();
-    if (searchString) {
-      searchMovies(searchString);
-    } else {
-      searchMovies('james-bond'); // fetch James Bond movies again if search string is empty
-    }
+    searchMovies();
   };
 
   return (
@@ -43,8 +38,7 @@ export default function Search() {
       {movies && movies.map((movie) => (
         <div key={movie.imdbID}>
           <h2>{movie.Title}</h2>
-          <p><strong>Regissør:</strong> {movie.Director}</p>
-          <p><strong>År publisert:</strong> {movie.Year}</p>
+          <p>{movie.Year}</p>
           <img src={movie.Poster} alt={movie.Title} />
         </div>
       ))}
